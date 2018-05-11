c53e2b843b4e98930b3ec2730c2b89b7804a0d30
son <pvanson@openwt.com>
2018-05-11 10:13:32 +0700

    first commit

---
+++ src/app/modules/welcome/welcome.vm.ts
@@ -0,0 +1,127 @@
+import { DialogService } from 'aurelia-dialog';
+import { autoinject, PLATFORM } from 'aurelia-framework';
+import {
+  validateTrigger, ValidationController, ValidationControllerFactory, ValidationRules
+} from 'aurelia-validation';
+
+import { EditPersonCustomElement } from '../../resources/elements/edit-person/edit-person.element';
+import { ShowPersonCustomElement } from '../../resources/elements/show-person/show-person.element';
+import { AppConfigService } from '../../services/app-config.service';
+import { GenericDialogService } from '../../services/generic-dialog.service';
+import { LanguageService } from '../../services/language.service';
+import { Logger, LogManager } from '../../services/logger.service';
+
+@autoinject
+export class WelcomeViewModel {
+  public heading: string = 'Welcome to the Aurelia Navigation App';
+  public firstName: string = 'John';
+  public lastName: string = 'Doe';
+  public previousValue: string = this.fullName;
+  public currentDate: Date = new Date();
+  public jsonProperty: {} = { key1: 'value1', key2: 'value2' };
+  public validationValid: boolean = false;
+
+  private logger: Logger;
+  private validationController: ValidationController;
+
+  constructor(
+    private appConfigService: AppConfigService,
+    private languageService: LanguageService,
+    private dialogService: DialogService,
+    validationControllerFactory: ValidationControllerFactory,
+    private genericDialogService: GenericDialogService,
+  ) {
+    this.logger = LogManager.getLogger('Welcome VM');
+    this.logger.info('appConfig => name:', this.appConfigService.getName());
+    this.logger.info('appConfig => version:', this.appConfigService.getVersion());
+    this.logger.info('appConfig => env:', this.appConfigService.getEnv());
+    this.logger.info('appConfig => platform:', this.appConfigService.getPlatform());
+    this.logger.info('appConfig => config:', this.appConfigService.getConfig());
+
+    this.validationController = validationControllerFactory.createForCurrentScope();
+    this.validationController.validateTrigger = validateTrigger.manual;
+  }
+
+  public canDeactivate(): boolean {
+    if (this.fullName !== this.previousValue) {
+      return confirm('Are you sure you want to leave?');
+    }
+
+    return true;
+  }
+
+  public validateFirstName(): void {
+    this.validationController
+      .validate({
+        object: this,
+        rules: ValidationRules.ensure('firstName').required().rules
+      })
+      .then(r => this.validationValid = r.valid);
+  }
+
+  public openDialog(): void {
+    const person = { firstName: 'Wade', middleName: 'Owen', lastName: 'Watts' };
+    this.dialogService.open({ viewModel: EditPersonCustomElement, model: person }).whenClosed(response => {
+      if (!response.wasCancelled) {
+        this.logger.info('Dialog not cancled', response);
+      } else {
+        this.logger.info('Dialog cancled', response);
+      }
+    });
+  }
+
+  // Getters can't be directly observed, so they must be dirty checked.
+  // However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
+  // To optimize by declaring the properties that this getter is computed from, uncomment the line below
+  // as well as the corresponding import above.
+  // @computedFrom('firstName', 'lastName')
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  public submit(): void {
+    this.previousValue = this.fullName;
+    alert(`Welcome, ${this.fullName}!`);
+  }
+
+  public switchLanguage(): void {
+    const lang = this.languageService.getCurrentLang();
+    if (lang === this.languageService.getSupportedLanguages()[0]) {
+      this.languageService.setLocale(this.languageService.getSupportedLanguages()[1]);
+    } else {
+      this.languageService.setLocale(this.languageService.getSupportedLanguages()[0]);
+    }
+  }
+
+  public openGenericDialog(): void {
+    const dialog = this.genericDialogService.showDialog<ShowPersonCustomElement>({
+      title: 'Zeige Person', // Can be a translation string
+      contentViewModel: PLATFORM.moduleName('resources/elements/show-person/show-person.element'),
+      contentModel: {
+        firstName: this.firstName
+      },
+      buttons: [
+        GenericDialogService.createCancelButton<ShowPersonCustomElement>(() => Promise.resolve()),
+        GenericDialogService.createSaveButton<ShowPersonCustomElement>(ele => {
+          this.logger.debug('Clicked on save in dialog', ele);
+
+          return Promise.resolve();
+        }, ele => ele.isValid)
+      ]
+    });
+
+    dialog.whenClosed(result => {
+      if (!result.wasCancelled) {
+        this.logger.debug('Dialog not canceld', result);
+      } else {
+        this.logger.debug('Dialog canceld', result);
+      }
+    });
+  }
+}
+
+export class UpperValueConverter {
+  public toView(value: string): string {
+    return value && value.toUpperCase();
+  }
+}
